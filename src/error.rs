d651d7da559ff0469482b4a4e483c0f8bed0882e
hasezoey <hasezoey@gmail.com>
2022-01-24 04:37:08 +0100

    feat(error::TemplateError): implement "Display" and "Error"

--- src/error.rs
+++ src/error.rs
@@ -26,3 +26,36 @@ impl TemplateError {
 		return self.kind;
 	}
 }
+
+impl std::fmt::Display for TemplateError {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		return write!(
+			f,
+			"{}",
+			match &self.kind {
+				TemplateErrorKind::MissingData => format!("MissingData: {}", self.error),
+			}
+		);
+	}
+}
+
+impl error::Error for TemplateError {}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn test_display() {
+		let missing_data_error = TemplateError::new(TemplateErrorKind::MissingData, "SomeText");
+
+		assert_eq!("MissingData: SomeText", format!("{}", missing_data_error));
+	}
+
+	#[test]
+	fn test_get_kind() {
+		let missing_data_error = TemplateError::new(TemplateErrorKind::MissingData, "SomeText");
+
+		assert_eq!(TemplateErrorKind::MissingData, missing_data_error.kind());
+	}
+}
